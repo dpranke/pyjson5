27375a04af983f47e1846efab522aef19f930711
Dirk Pranke <dpranke@gmail.com>
2024-02-19 17:52:21 -0800

    Drop Python2 versions from setup.py.

--- setup.py
+++ setup.py
@@ -54,8 +54,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',

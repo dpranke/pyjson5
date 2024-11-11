470225509783acdc61287801faad39d9dc1fe3c3
Dirk Pranke <dpranke@gmail.com>
2024-11-11 10:43:56 -0800

    Bump version to 0.9.29_dev.

--- json5/version.py
+++ json5/version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.9.28'
+__version__ = '0.9.29_dev'
 
 # For backward-compatibility with earlier versions of json5:
 VERSION = __version__

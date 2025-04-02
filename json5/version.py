ceb8d42ca49553790fca1c45a05bd13e053bf971
Dirk Pranke <dpranke@gmail.com>
2025-04-01 21:05:48 -0700

    Bump the version to 0.11.0

--- json5/version.py
+++ json5/version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.11.0.dev0'
+__version__ = '0.11.0'
 
 # For backward-compatibility with earlier versions of json5:
 VERSION = __version__
